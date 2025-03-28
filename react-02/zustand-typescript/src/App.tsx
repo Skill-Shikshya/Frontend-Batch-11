d907d4d8eb43391e6a78f0767bc8bd51dffd3d31
DK <dk@vrittechnologies.com>
2025-03-28 15:31:05 +0545

    changes made in Zustand-typescript

--- react-02/zustand-typescript/src/App.tsx
+++ react-02/zustand-typescript/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   console.log(products);
   useEffect(() => {
     fetchProducts();
-  }, []);
+  }, [fetchProducts]);
 
   // const { count, inc, dec } = useCountStore();
 
@@ -54,6 +54,23 @@ function App() {
         >
           Decrement Count
         </button> */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
+          {products.length > 0 &&
+            products.map((product) => (
+              <div key={product.id} className="border rounded-lg p-4 shadow-lg">
+                <img
+                  src={product?.image}
+                  alt={product?.title}
+                  className="h-40 w-full object-contain"
+                />
+                <h3 className="text-lg font-semibold mt-2">{product?.title}</h3>
+                <p className="text-gray-500">{product?.category}</p>
+                <p className="text-blue-600 font-bold">${product?.price}</p>
+                <p>{product?.rating?.rate}</p>
+                <p>Rating Count {product?.rating?.count}</p>
+              </div>
+            ))}
+        </div>
       </div>
     </>
   );
